1448736fd1d226456c077037c088e6416a937a9a
Andrey Nikonov <a.einomi@gmail.com>
2017-10-23 00:48:47 +0300

    feat: Add state syncing

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { StyleSheet, Text, View, StatusBar, AppState, AsyncStorage } from 'react-native'
 import { TabNavigator } from 'react-navigation'
 import { Constants } from 'expo'
 import { Foundation } from '@expo/vector-icons'
@@ -26,7 +26,47 @@ const Tabs = TabNavigator({
 });
 
 export default class App extends React.Component {
+    state = {
+        isStoreLoading: false,
+        store: store
+    };
+
+    componentWillMount() {
+        var self = this;
+        AppState.addEventListener('change', this._handleAppStateChange.bind(this));
+        this.setState({isStoreLoading: true});
+        AsyncStorage.getItem('completeStore').then((value)=>{
+            if(value && value.length){
+                let initialStore = JSON.parse(value);
+                self.setState({store: createStore(reducers, initialStore, middleware)});
+            }else{
+                self.setState({store: store});
+            }
+            self.setState({isStoreLoading: false});
+        }).catch((error)=>{
+            self.setState({store: store});
+            self.setState({isStoreLoading: false});
+        });
+    }
+
+    componentWillUnmount() {
+        AppState.removeEventListener('change', this._handleAppStateChange.bind(this));
+    }
+
+    _handleAppStateChange(currentAppState) {
+        let storingValue = JSON.stringify(this.state.store.getState());
+        AsyncStorage.setItem('completeStore', storingValue);
+    }
+
     render() {
+        if (this.state.isStoreLoading) {
+            return (
+                <View style={styles.loading}>
+                    <Text style={styles.loadingText}>Loading...</Text>
+                </View>
+            );
+        }
+
         return (
             <Provider store={store}>
                 <View style={styles.container}>
@@ -41,6 +81,15 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
+    loading: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    loadingText: {
+        fontSize: 16,
+        fontWeight: '500'
+    },
     container: {
         flex: 1,
         backgroundColor: '#fff',
